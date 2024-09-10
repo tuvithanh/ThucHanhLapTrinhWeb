7902fe1135d28b5557d824e7e9cae6c727ed13e2
tuvithanh <61571748+tuvithanh@users.noreply.github.com>
2024-09-10 22:49:07 +0700

    quick update

---
+++ bai2table1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using bai2table1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace bai2table1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult tableA()
+        {
+            return View();
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ bai2table1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bai2table1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
