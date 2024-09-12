dc22efde705373379a2dba7330d2dc78feddceb6
tuvithanh <61571748+tuvithanh@users.noreply.github.com>
2024-09-12 23:41:54 +0800

    quickupdate

--- bai2table1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace bai2table1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
