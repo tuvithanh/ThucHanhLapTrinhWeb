05c555327a63a23e17176c7005588e6f7505a116
tuvithanh <61571748+tuvithanh@users.noreply.github.com>
2024-10-13 17:20:24 +0800

    quick update

---
+++ Online1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult TrangChu2()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Online1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online1.MvcApplication" Language="C#" %>
