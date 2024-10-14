e6122f4ff1e711c8bc5bd499b759a3482961ae02
tuvithanh <61571748+tuvithanh@users.noreply.github.com>
2024-10-14 23:56:02 +0700

    quick update

---
+++ 23DH113217_MyStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH113217_MyStore.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+    }
+}
---
+++ 23DH113217_MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH113217_MyStore.MvcApplication" Language="C#" %>
